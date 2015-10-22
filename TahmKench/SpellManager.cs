abce848361119342e04a574cb172ba98b5340640
VetoLeagueSharp <i1023404@hotmail.com>
2015-10-22 03:50:05 +0100

    Tahm Kench 1.0
    In a folder this time

---
+++ TahmKench/SpellManager.cs
@@ -0,0 +1,32 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace TahmKench
+{
+    public class SpellManager
+    {
+        private static Obj_AI_Hero Player = ObjectManager.Player;
+
+        public static Spell Q, W, W2, E, R;
+
+        public static void Initialise()
+        {
+            Q = new Spell(SpellSlot.Q, 800);
+            Q.SetSkillshot(.1f, 75, 2000, true, SkillshotType.SkillshotLine);
+
+            W = new Spell(SpellSlot.W, 250);
+            W2 = new Spell(SpellSlot.W, 900);
+            W2.SetSkillshot(.1f, 75, 900, true, SkillshotType.SkillshotLine);
+
+            E = new Spell(SpellSlot.E);
+
+            R = new Spell(SpellSlot.R, 4000);
+        }
+
+        public static void CastSpell(Spell spell, Obj_AI_Base target, HitChance hitchance)
+        {
+            if (target.IsValidTarget(spell.Range) && spell.GetPrediction(target).Hitchance >= hitchance)
+                spell.Cast(target);
+        }
+    }
+}
---
+++ TahmKench/TahmKench.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Color = System.Drawing.Color;
+
+namespace TahmKench
+{
+    class TahmKench
+    {
+        public static Obj_AI_Hero Player = ObjectManager.Player;
+        public static SwallowedTarget current = SwallowedTarget.None; // Credits to Nouser
+        public static string tahmPassive = "TahmKenchPDebuffCounter";
+
+        public enum SwallowedTarget
+        {
+            Ally,
+            Enemy,
+            Minion,
+            None
+        }
+
+        internal static void Load(EventArgs args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            if (Player.ChampionName != "TahmKench")
+                return;
+
+            SpellManager.Initialise();
+            MenuConfig.LoadMenu();
+
+            Game.OnUpdate += Game_OnUpdate;
+            Drawing.OnDraw += Drawing_OnDraw;
+        }
+
+        static void Game_OnUpdate(EventArgs args)
+        {
+            if (Player.IsDead || args == null)
+                return;
+
+            KillSteal();
+
+            switch (MenuConfig.Orbwalker.ActiveMode)
+            {
+                case Orbwalking.OrbwalkingMode.Combo:
+                    Combo();
+                    break;
+
+                case Orbwalking.OrbwalkingMode.Mixed:
+                    Harass();
+                    break;
+
+                case Orbwalking.OrbwalkingMode.LaneClear:
+                    // LaneClear();
+                    break;
+
+                case Orbwalking.OrbwalkingMode.LastHit:
+                    LastHit();
+                    break;
+            }
+        }
+
+        private static void Combo()
+        {
+            var target = TargetSelector.GetTarget(SpellManager.Q.Range, TargetSelector.DamageType.Physical);
+            var closestMinion = ObjectManager.Get<Obj_AI_Minion>().Where(enemy => enemy.IsEnemy && enemy.Distance(Player) < 250).FirstOrDefault();
+
+            if (target != null)
+            {
+                int buffStack = target.GetBuffCount(tahmPassive);
+                switch (buffStack)
+                {
+                    case 1:
+                        
+
+                    case 2:
+                        if (MenuConfig.ComboQ)
+                            SpellManager.Q.Cast(target);
+                        else if (target.Distance(Player) <= 200)
+                            Player.IssueOrder(GameObjectOrder.AttackUnit, target);
+                        else if (MenuConfig.ComboW && target.Distance(Player) > 350 && buffStack < 2)
+                        {
+                            if (current == SwallowedTarget.None)
+                                SpellManager.W.CastOnUnit(closestMinion);
+                            else if (current == SwallowedTarget.Minion)
+                                SpellManager.W2.CastIfHitchanceEquals(target, HitChance.High);
+                        }
+                        break;
+
+                    case 3:
+                        if (MenuConfig.ComboQ)
+                            SpellManager.Q.Cast(target);
+
+                        if (current == SwallowedTarget.None && MenuConfig.ComboW && target.Distance(Player) < 250)
+                            SpellManager.W.CastOnUnit(target);
+                        break;
+                }
+            }
+        }
+
+        private static void Harass()
+        {
+            if (Player.ManaPercent < MenuConfig.HarassMana)
+                return;
+
+            if (SpellManager.Q.IsReady() && MenuConfig.HarassQ)
+            {
+                var target = TargetSelector.GetTarget(SpellManager.Q.Range, TargetSelector.DamageType.Physical);
+
+                if (target != null)
+                    SpellManager.CastSpell(SpellManager.Q, target, HitChance.High);
+            }
+
+        }
+
+        /*
+        private static void LaneClear()
+        {
+            // Gonna figure out a laneclear method for Tahm
+        }
+        */
+
+        private static void LastHit()
+        {
+            if (Player.ManaPercent < MenuConfig.LastHitMana)
+                return;
+
+            if (SpellManager.Q.IsReady() && MenuConfig.LastHitQ)
+            {
+                var Minion = MinionManager.GetMinions(SpellManager.Q.Range, MinionTypes.All, MinionTeam.Enemy)
+                    .FirstOrDefault(minion => minion.IsValidTarget(SpellManager.Q.Range) &&
+                    Player.GetSpellDamage(minion, SpellSlot.Q) >= minion.Health);
+
+                if (Minion != null)
+                    SpellManager.CastSpell(SpellManager.Q, Minion, HitChance.High);
+            }
+        }
+
+        private static void KillSteal()
+        {
+            var target = ObjectManager.Get<Obj_AI_Hero>()
+                .FirstOrDefault(enemy => enemy.IsValidTarget(SpellManager.Q.Range) && enemy.Health < Player.GetSpellDamage(enemy, SpellSlot.Q));
+
+            if (target.IsValidTarget(SpellManager.Q.Range))
+                SpellManager.Q.CastIfHitchanceEquals(target, HitChance.High);
+        }
+
+        private static void AutoShield()
+        {
+            if (MenuConfig.AutoShieldToggle && Player.Health <= MenuConfig.AutoShieldHP)
+                SpellManager.E.Cast();
+        }
+
+        static void Drawing_OnDraw(EventArgs args)
+        {
+            if (MenuConfig.DrawQ)
+                Render.Circle.DrawCircle(Player.Position, SpellManager.Q.Range, Color.Red);
+
+            if (MenuConfig.DrawW)
+            {
+                if (current == SwallowedTarget.None)
+                    Render.Circle.DrawCircle(Player.Position, SpellManager.W.Range, Color.LightBlue);
+                else if (current == SwallowedTarget.Minion)
+                    Render.Circle.DrawCircle(Player.Position, SpellManager.W2.Range, Color.DarkBlue);
+            }
+        }
+    }
+}
